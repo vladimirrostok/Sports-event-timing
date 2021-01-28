6b5bffd312b373289f4a5a3d50121b0ac068e746
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2021-01-28 23:51:03 +0200

    Apply cmd pattern to main.go file, add utils package for DB connection

---
+++ app/Go/srv/utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+// GetDBConnection with the given configuration details.
+func GetDBConnection(driver, username, password, port, host, database string) (*gorm.DB, error) {
+	var err error
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, username, database, password)
+	DB, err := gorm.Open(driver, DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database", driver)
+		log.Fatal("Error:", err)
+	} else {
+		fmt.Printf("Connected to the %s database \n", driver)
+	}
+
+	return DB, nil
+}
