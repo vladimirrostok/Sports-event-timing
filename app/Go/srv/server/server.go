c781c7e1a65dd9c870973827a2ef273997f91474
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2021-02-01 01:47:05 +0200

    Remove leftover piece of HTTPClient interface code

--- app/Go/srv/server/server.go
+++ app/Go/srv/server/server.go
@@ -3,16 +3,8 @@ package server
 import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
-	"io"
-	"net/http"
 	"sports/backend/srv/controllers/dashboard"
 )
-
-// HTTPClient interface to mock the network requests for test purposes.
-type HTTPClient interface {
-	Do(req *http.Request) (*http.Response, error)
-	Post(url, contentType string, body io.Reader) (*http.Response, error)
-}
 
 // Server is a wrapper for the service context.
 type Server struct {
