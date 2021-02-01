82e169197de49c743b51d030fd35d6d07d1043c8
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2021-02-02 00:35:17 +0200

    Make results table update existing row with finish time when it comes.

--- app/React/dashboard/src/app/pages/Dashboard/index.js
+++ app/React/dashboard/src/app/pages/Dashboard/index.js
@@ -1,13 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Results from "../../components/Results";
 
 const baseURL = "wss://localhost:8000/dashboard";
 
 const Dashboard = () => {
   const [results, setResults] = useState([]);
+  const [ws, setWs] = useState(null);
+
+  const updateResults = useCallback(
+    (msg) => {
+      const newResult = JSON.parse(msg.data);
+
+      // Check if a new message received has the same result ID.
+      const isNew = !results.find((p) => p.id === newResult.id);
+
+      // If ID is different, then append the existing results with a new row.
+      // If ID is same, then grab time_finish and update existing row with the same result ID.
+      if (isNew) {
+        setResults([...results, newResult]);
+      } else {
+        const newResults = results.map((p) =>
+          p.id === newResult.id
+            ? { ...p, time_finish: newResult.time_finish }
+            : p
+        );
+        setResults(newResults);
+      }
+    },
+    [results, setResults]
+  );
 
   useEffect(() => {
-    let ws = new WebSocket(baseURL);
+    const ws = new WebSocket(baseURL);
 
     ws.onopen = (evt) => {
       console.log("WebSocket opened!", { evt });
@@ -15,30 +39,33 @@ const Dashboard = () => {
 
     ws.onclose = (evt) => {
       console.log("WebSocket closed!", { evt });
-      ws = undefined;
-    };
-
-    ws.onmessage = (msg) => {
-      console.log("Websocket message:", { msg });
-      updateResults(msg);
+      ws.current = undefined;
     };
 
     ws.onerror = (err) => {
       console.log("Websocket error:", { err });
     };
+
+    setWs(ws);
+
+    return () => {
+      ws.close();
+    };
   }, []);
+
+  useEffect(() => {
+    if (ws) {
+      ws.onmessage = updateResults;
+    }
+  }, [ws, updateResults]);
 
   return (
     <div className="dashboard">
-      <h1>Sports events dashboard</h1>
+      <h1>Sports events dashboard ({results.length} elements)</h1>
       {results && <Results results={results} />}
       {console.log(results)}
     </div>
   );
-
-  function updateResults(msg) {
-    setResults((prevArray) => [...prevArray, JSON.parse(msg.data)]);
-  }
 };
 
 export default Dashboard;
