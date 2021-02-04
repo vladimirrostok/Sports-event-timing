8b0114177d69e07d2b63a818d975f912264049e5
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2021-02-04 05:22:42 +0200

    Rework table state hook to fetch array of objects instead of object

--- app/React/dashboard/src/app/pages/Dashboard/index.js
+++ app/React/dashboard/src/app/pages/Dashboard/index.js
@@ -9,38 +9,46 @@ const Dashboard = () => {
 
   const updateResults = useCallback(
     (msg) => {
-      const newResult = JSON.parse(msg.data);
-
-      // Handle null message which may come from the server when there are no results available
-      if (newResult != null) {
-        if (newResult.time_finish) {
-          var fulldate = new Date(newResult.time_finish);
-          var formattedDate =
-            fulldate.getHours() +
-            ":" +
-            fulldate.getMinutes() +
-            ":" +
-            fulldate.getSeconds();
-          var time = formattedDate + "." + fulldate.getMilliseconds();
-          newResult.time_finish = time;
-        } else {
-          newResult.time_finish = "";
+      // Parse the results JSON response.
+      const data = JSON.parse(msg.data);
+      // If data is not null then use it.
+      if (data) {
+        // Format time when data arrives (new data on just opened page) or new message.
+        for (var i = 0; i < data.length; i++) {
+          // If result has time_finish (UNIX time) then convert it into readable time.
+          if (data[i].time_finish) {
+            var fulldate = new Date(data[i].time_finish);
+            data[i].time_finish = `${fulldate.getHours()}:${fulldate.getMinutes()}:${fulldate.getSeconds()}.${fulldate.getMilliseconds()}`;
+          } else {
+            data[i].time_finish = "";
+          }
         }
 
-        // Check if a new message received has the same result ID.
-        const isNew = !results.find((p) => p.id === newResult.id);
+        // If there are no results on page yet, use all data from the server.
+        if (!results.length) {
+          // If it's initial data then it's array, if that's a message then it's object.
+          if (Array.isArray(data)) {
+            setResults(data);
+          } else {
+            setResults([data, ...results]);
+          }
+        }
 
-        // If ID is different, then append the existing results with a new row.
-        // If ID is same, then grab time_finish and update existing row with the same result ID.
-        if (isNew) {
-          setResults([newResult, ...results]);
-        } else {
-          const newResults = results.map((p) =>
-            p.id === newResult.id
-              ? { ...p, time_finish: newResult.time_finish }
-              : p
-          );
-          setResults(newResults);
+        // If there are messages in client state already, append these with new data from message.
+        if (results.length) {
+          // Check if a new message has the same result ID.
+          const isNew = !results.find((p) => p.id === data.id);
+
+          // If ID is different, then append the existing results with a new row.
+          // If ID is same, then grab time_finish and update existing row with the same result ID.
+          if (isNew) {
+            setResults([data, ...results]);
+          } else {
+            const newResults = results.map((p) =>
+              p.id === data.id ? { ...p, time_finish: data.time_finish } : p
+            );
+            setResults(newResults);
+          }
         }
       }
     },
